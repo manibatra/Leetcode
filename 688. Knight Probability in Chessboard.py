4e25bfe41e1c7d0ca5b51050f11e7015692cbdd5
Mani <manibatra2002@gmail.com>
2018-05-22 11:23:22 +1000

    correct solved

--- 688. Knight Probability in Chessboard.py
+++ 688. Knight Probability in Chessboard.py
@@ -1,39 +1,28 @@
 class Solution:
-	total_paths = 0
-	off_board = 0
-	dp = []
 	def knightProbability(self, N, K, r, c):
-		self.total_paths = 0
-		self.off_board = 0
-		self.dp = [[-1 for j in range(N)] for i in range(N)]
-		self.findPaths(N, K, r, c, 0)
-		#print(self.total_paths - self.off_board, 8**K)
-		return (self.total_paths - self.off_board)/(8**K)
+		dp = [[[0 for k in range(K + 1)] for j in range(N)] for i in range(N)]
+		return self.findPaths(N, K, r, c, 0, dp)/(8**K)
+
 		
-	def findPaths(self, N, K, r, c, steps):
+	def findPaths(self, N, K, r, c, steps, dp):
 		if r >= N or c >= N:
-			self.off_board += 1
-			self.total_paths += 1
-			return
+			return 0
 		if r < 0 or c < 0:
-			self.off_board += 1
-			self.total_paths += 1
-			return
+			return 0
+		if steps != 0 and dp[r][c][steps - 1] != 0:
+			return dp[r][c][steps - 1]
 		if steps == K:
-			self.total_paths += 1
-			self.dp[r][c] = (self.off_board, self.total_paths)
-			return
-		if self.dp[r][c] != -1:
-			self.off_board += self.dp[r][c][0]
-			self.total_paths += self.dp[r][c][1]
-			return 
+			return 1
+		
 		x = [1, 2]
 		y = [1, -1]
 		for i in x:
 			for j in x:
 				for k in y:
 					if i != j:
-						self.findPaths(N, K, r + i * k, c + j * k, steps + 1)
-						self.findPaths(N, K, r + i * k, c + j * k * -1, steps + 1)
+						dp[r][c][steps - 1] += self.findPaths(N, K, r + i * k, c + j * k, 
+						steps + 1, dp)
+						dp[r][c][steps - 1] += self.findPaths(N, K, r + i * k, c + j * k * -1, 
+						steps + 1, dp)
 					
-		return
+		return dp[r][c][steps - 1]
